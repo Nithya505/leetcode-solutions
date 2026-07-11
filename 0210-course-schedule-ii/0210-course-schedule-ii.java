b26854acf5ed44fab6cae111f12705b7a710b651
Nithya <amudalanithya@gmail.com>
2026-07-11 11:01:08 +0530

    Time: 5 ms (83.37%), Space: 47 MB (40.04%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+        // adjacency list
+        List<List<Integer>> graph = new ArrayList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // indegree array
+        int[] indegree = new int[numCourses];
+
+        // build graph
+        for (int[] pre : prerequisites) {
+            int a = pre[0];
+            int b = pre[1];
+
+            graph.get(b).add(a); // b -> a
+            indegree[a]++;
+        }
+
+        // queue for courses having indegree 0
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+       int[] ans=new int[numCourses];
+       int idx=0;
+
+        // BFS (Kahn's Algorithm)
+        while (!q.isEmpty()) {
+
+            int curr = q.poll();
+            ans[idx++]=curr;
+
+            for (int nei : graph.get(curr)) {
+
+                indegree[nei]--;
+
+                if (indegree[nei] == 0) {
+                    q.offer(nei);
+                }
+            }
+        }
+
+        if(idx!=numCourses){
+            return new int[0];
+        }
+        return ans;
+    }
+}
